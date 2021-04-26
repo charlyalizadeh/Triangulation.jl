f5fb2cf2ab5b8cf8a2393ea110fab97093fbdff0
charlyalizadeh <charlypro1502@outlook.fr>
2021-04-26 21:03:14 +0200

    Add tests for LEX_M and MCS_M

--- test/Triangulation.jl
+++ test/Triangulation.jl
@@ -8,25 +8,40 @@ using Main.Triangulation.LightGraphs; const lg = Triangulation.LightGraphs
 # Very poor test coverage
 # TODO: Better test
 @testset "EG" begin
-    g = lg.path_graph(5000)
-    lg.add_edge!(g, 1, 5000)
+    g = lg.cycle_graph(1000)
     @test !ischordal(g)
     trg.EG!(g)
     @test ischordal(g)
 end
 
+
 @testset "MD" begin
-    g = lg.path_graph(5000)
-    lg.add_edge!(g, 1, 5000)
+    g = lg.cycle_graph(1000)
     @test !ischordal(g)
     trg.MD!(g)
     @test ischordal(g)
 end
 
+
 @testset "Cholesky" begin
-    g = lg.path_graph(5000)
-    lg.add_edge!(g, 1, 5000)
+    g = lg.cycle_graph(1000)
     @test !ischordal(g)
     g, data = trg.choleskydec(g)
+    @test ischordal(g)
+end
+
+
+@testset "LEX_M" begin
+    g = lg.cycle_graph(1000)
+    @test !ischordal(g)
+    data = trg.LEX_M!(g)
+    @test ischordal(g)
+end
+
+
+@testset "MCS_M" begin
+    g = lg.cycle_graph(1000)
+    @test !ischordal(g)
+    g, data = trg.MCS_M(g)
     @test ischordal(g)
 end
