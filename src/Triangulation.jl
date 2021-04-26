f3662e8f32a5861e3d4c921f8335f55500769219
charlyalizadeh <charlypro1502@outlook.fr>
2021-04-26 20:31:40 +0200

    Add LEX-M algorithm

--- src/Triangulation.jl
+++ src/Triangulation.jl
@@ -1,17 +1,21 @@
 module Triangulation
 
+
 using LightGraphs
 using MetaGraphs
 using Combinatorics
 using LinearAlgebra
 using SparseArrays
 
-export EG, EG!
-       MD, MD!
-       choleskydec
-
 include("./EG.jl")
 include("./MD.jl")
+include("./LEX_M.jl")
 include("./Cholesky.jl")
+
+export EG, EG!
+       MD, MD!
+       choleskydec,
+       LEX_M, LEX_M!,
+
 
 end # module
