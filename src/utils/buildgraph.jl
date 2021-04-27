984f1300dcaefe2c4adbdb447b27598aa730a055
charlyalizadeh <charlypro1502@outlook.fr>
2021-04-27 18:20:21 +0200

    Add: convert array-based cliquetree to MetaGraph

--- src/utils/buildgraph.jl
+++ src/utils/buildgraph.jl
@@ -1,7 +1,24 @@
-function buildmeta(g)
+function buildmeta(g::AbstractGraph)
     mg = MetaGraph(g)
     for v in vertices(mg)
         set_indexing_prop!(mg, v, :v, v)
+    end
+    return mg
+end
+
+function build_graph_cliquetree(cliques::AbstractVector, cliquetree::AbstractVector)
+    mg = MetaGraph(length(cliques))
+    set_indexing_prop!(mg, :clique)
+    for (i, c) in enumerate(cliques)
+        set_indexing_prop!(mg, i, :clique, c)
+    end
+    for edge in cliquetree
+        c1 = cliques[edge[1]]
+        c2 = cliques[edge[2]]
+        src = mg[src, :clique]
+        dst = mg[dst, :clique]
+        add_edge!(mg, src, dst)
+        set_prop!(mg, src, dst, :weight, length(intersect(c1, c2)))
     end
     return mg
 end
