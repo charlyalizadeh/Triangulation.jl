d01ae5d20ec4e233a9d6f4cda5bd67429780ebdd
charlyalizadeh <charlypro1502@outlook.fr>
2021-04-27 19:47:02 +0200

    Fix: D.K. Molzahn name typo, sry

--- src/MergeAlgos/molzahn.jl
+++ src/MergeAlgos/molzahn.jl
@@ -1,11 +1,11 @@
-function minimize_molzhan(cliques::AbstractVector, cliquetree::AbstractVector)
-    deltas = [molzhan_heuristic(cliques, edge) for edge in cliquetree]
+function minimize_molzahn(cliques::AbstractVector, cliquetree::AbstractVector)
+    deltas = [molzahn_heuristic(cliques, edge) for edge in cliquetree]
     index = argmin(deltas)
     return cliquetree[index]
 end
 
 
-function molzhan_heuristic(cliques::AbstractVector, edge)
+function molzahn_heuristic(cliques::AbstractVector, edge)
     size_intersect = length(intersect(cliques[edge[1]], cliques[edge[2]]))
     size_clique1 = length(cliques[edge[1]])
     size_clique2 = length(cliques[edge[2]])
